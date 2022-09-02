fcaab6180a37d9703690ac6f25545772a6863c06
Mostafa Khaled <75026494+Mostafa-Khaled@users.noreply.github.com>
2022-09-03 01:11:18 +0200

    Hangman

---
+++ src/assets/words.js
@@ -0,0 +1,17 @@
+const ENGLISH_WORDS = [
+    'their',
+    'would',
+    'about',
+    'there',
+    'think',
+    'which',
+    'people',
+    'could',
+    'other',
+    'these',
+    'first',
+    'thing',
+    'those',
+    'woman',
+    'child']
+export default ENGLISH_WORDS; 
